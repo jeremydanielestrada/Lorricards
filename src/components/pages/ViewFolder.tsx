8264fbead234bf4b06768fe82f849f063fb3fce2
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-11-22 19:44:16 +0800

    add: text area for note

--- src/components/pages/ViewFolder.tsx
+++ src/components/pages/ViewFolder.tsx
@@ -6,10 +6,20 @@ function ViewFolder() {
   const folderData = location.state?.folderData;
 
   return (
-    <div className="text-center">
-      <h1>{folderData?.title || title}</h1>
-      <p>ID: {folderData?.id}</p>
-      {/* Access all folder properties */}
+    <div className="mx-auto w-80 sm:w-100  md:w-180">
+      <h1 className="text-center  text-3xl sm:text-5xl md:text-7xl">
+        {folderData?.title || title}
+      </h1>
+
+      <div className="mt-5">
+        <form>
+          <textarea
+            className="input-base m-0 resize-none "
+            placeholder="Paste your notes.."
+          ></textarea>
+          <button className="base-btn mt-1">Save</button>
+        </form>
+      </div>
     </div>
   );
 }
