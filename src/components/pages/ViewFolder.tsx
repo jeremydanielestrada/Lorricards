d0c843af5b263cbe7e6f7c3f08b57025aec06d9b
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-11-23 19:45:08 +0800

    feat: add tab for paste and import notes

--- src/components/pages/ViewFolder.tsx
+++ src/components/pages/ViewFolder.tsx
@@ -1,9 +1,44 @@
+import { useState, type JSX } from "react";
 import { useParams, useLocation } from "react-router";
+import { NotebookPen, Import, type LucideIcon } from "lucide-react";
+
+//Types
+type TabId = "tab1" | "tab2";
+interface Tabs {
+  id: TabId;
+  label: string;
+  icon: LucideIcon;
+}
 
 function ViewFolder() {
   const location = useLocation();
   const { title } = useParams();
   const folderData = location.state?.folderData;
+  const [activeTab, setActiveTab] = useState<TabId>("tab1");
+
+  const tabs: Tabs[] = [
+    { id: "tab1", label: "Paste Notes", icon: NotebookPen },
+    { id: "tab2", label: "Super Import", icon: Import },
+  ];
+
+  const tabContent: Record<TabId, JSX.Element> = {
+    tab1: (
+      <form>
+        <textarea
+          className="input-base m-0 resize-none "
+          placeholder="Paste your notes.."
+        ></textarea>
+        <button className="base-btn mt-1">Save</button>
+      </form>
+    ),
+
+    tab2: (
+      <div className="mt-2">
+        <h2 className="text-xl font-bold mb-2">Import File</h2>
+        <input type="file" className="input-base" />
+      </div>
+    ),
+  };
 
   return (
     <div className="mx-auto w-80 sm:w-100  md:w-180">
@@ -11,15 +46,26 @@ function ViewFolder() {
         {folderData?.title || title}
       </h1>
 
-      <div className="mt-5">
-        <form>
-          <textarea
-            className="input-base m-0 resize-none "
-            placeholder="Paste your notes.."
-          ></textarea>
-          <button className="base-btn mt-1">Save</button>
-        </form>
+      <div className="mt-5 flex flex-wrap space-x-2 justify-center items-center border-b  font-semibold">
+        {tabs.map((tab) => {
+          const Icon = tab.icon;
+          return (
+            <button
+              className={`py-2 px-1 bg-slate-400 mb-2 rounded cursor-pointer font-semibold flex items-center space-x-1 ${
+                activeTab === tab.id
+                  ? " border-b-2 border-b-blue-600 text-blue-600"
+                  : `text-white`
+              }`}
+              key={tab.id}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              <span>{tab.label}</span>
+              <Icon />
+            </button>
+          );
+        })}
       </div>
+      <div className="mt-5">{tabContent[activeTab]}</div>
     </div>
   );
 }
