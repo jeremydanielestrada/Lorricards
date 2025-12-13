33cadc8d755a369fa8e18ecd0085ba324ced3cfe
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-13 11:47:46 +0800

    refactor: add edit and delete icon on folder

--- src/components/system/Folder.tsx
+++ src/components/system/Folder.tsx
@@ -1,4 +1,4 @@
-import { FolderClosed } from "lucide-react";
+import { FolderClosed, Pencil, Trash } from "lucide-react";
 import { NavLink } from "react-router";
 
 interface FolderTypes {
@@ -9,18 +9,27 @@ interface FolderTypes {
 
 function Folder({ title, link, folder }: FolderTypes) {
   return (
-    <NavLink
-      to={link}
-      state={{ folderData: folder }}
-      className={({ isActive }) =>
-        `py-2 my-2 rounded-sm cursor-pointer flex items-center ${
-          isActive ? "bg-slate-600" : "hover:bg-slate-500/75"
-        }`
-      }
-    >
-      <FolderClosed size="30" className="ml-2" />
-      <p className="text-md font-semibold mx-1 truncate">{title}</p>
-    </NavLink>
+    <div className="flex items-center justify-between">
+      <NavLink
+        to={link}
+        state={{ folderData: folder }}
+        className={({ isActive }) =>
+          `py-2 my-2 rounded-sm cursor-pointer flex items-center justify-between ${
+            isActive ? "bg-slate-600" : "hover:bg-slate-500/75"
+          }`
+        }
+      >
+        <div className="flex items-center">
+          <FolderClosed size="30" className="ml-2" />
+          <p className="text-md font-semibold mx-1 w-30 truncate">{title}</p>
+        </div>
+      </NavLink>
+
+      <div className="flex space-x-1 mx-2">
+        <Pencil />
+        <Trash />
+      </div>
+    </div>
   );
 }
 
