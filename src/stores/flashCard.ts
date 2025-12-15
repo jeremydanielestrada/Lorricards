a6a723a03aa0d49e3392bb6fb9a1a67685b836f7
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-15 09:24:41 +0800

    feat: add flasCard store

---
+++ src/stores/flashCard.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { create } from "zustand";
+import { api } from "../utils/axios";
+
+interface FlashCard {
+  question: string;
+  answer: string;
+}
+interface FlashCardResponse {
+  success: boolean;
+  message: string;
+  flashcards?: FlashCard;
+}
+
+interface FlashCardStore {
+  flashCards: FlashCard[];
+  getFlashCardByFolderId: (folderId: number) => Promise<void>;
+  createFlashCardsFromDocument: (
+    folderId: number,
+    document: string
+  ) => Promise<FlashCardResponse>;
+}
+
+export const useFlashCardStore = create<FlashCardStore>((set) => ({
+  flashCards: [],
+
+  getFlashCardByFolderId: async (folderId: number) => {
+    try {
+      const res = await api.get(`/get-flash-cards/${folderId}`);
+      set({ flashCards: res.data });
+    } catch (error: any) {
+      console.log(error);
+    }
+  },
+
+  createFlashCardsFromDocument: async (folderId: number, document: string) => {
+    try {
+      const res = await api.post(`/create-flash-card/${folderId}`, document);
+      return { success: true, ...res.data };
+    } catch (error: any) {
+      console.log(error);
+      return {
+        success: false,
+        message: error.response?.data?.message,
+      };
+    }
+  },
+}));
