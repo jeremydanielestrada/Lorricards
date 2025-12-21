bc55836bfd80280df502f3549ad59f553b36f268
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-21 09:24:09 +0800

    feat: add color indicator to answers

--- src/components/system/Flashcard.tsx
+++ src/components/system/Flashcard.tsx
@@ -7,7 +7,9 @@ function Flashcard({ question, answer }: Flashcard) {
   return (
     <div className="card border border-slate-400/35 m-2">
       <h2 className="text-lg font-extrabold border-b-slate-600">{question}</h2>
-      <p className="font-semibold">answer: {answer}</p>
+      <p className="font-semibold">
+        <span className="text-blue-500">Answer</span>: {answer}
+      </p>
     </div>
   );
 }
