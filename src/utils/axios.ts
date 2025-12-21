de6aa5a4f4157b83c2c2a5fa79785558f44d89a6
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-21 21:48:25 +0800

    feat: fix api url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_API_URL}`,
+  baseURL:
+    import.meta.env.VITE_BASE_API_URL ||
+    "https://lorricards-backend.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
